a0dc1b229ebca694615826d723e649799f5c282f
David Moore <mooredvd@live.ie>
2017-05-21 16:57:45 +0100

    urllib versus requests and ssl

--- news.py
+++ news.py
@@ -8,6 +8,7 @@ Created on Sat May 20 09:51:51 2017
 import feedparser
 from bs4 import BeautifulSoup
 import urllib.request
+import requests
 from nltk import word_tokenize
 from nltk.tokenize import RegexpTokenizer
 from nltk.tokenize import StanfordTokenizer
@@ -49,6 +50,19 @@ def web_scrape(web_index):
     soup=BeautifulSoup(page, 'lxml')
     return soup
 
+def web_scrape_alt(web_index):
+    base_path= web_index
+    web_path = base_path
+    print (web_path)
+    #Open and read web page. 
+    #Parse returned webpage with BeautifulSoup and extract all the tables in it. 
+    response = requests.get(web_path, verify=True)
+    page = response.content      
+    soup=BeautifulSoup(page, 'lxml')
+    return soup
+
+
+
 
     #If encoding cannot be determined exit. #
     #Site declares to be utf-8 so UnicodeDecodeError should not happen - we will see#
@@ -82,7 +96,7 @@ BBC_FEED = 'http://feeds.bbci.co.uk/news/rss.xml'
 feed = feedparser.parse(BBC_FEED)
 first_article = feed['entries'][0]
 
-obj = web_scrape(first_article['link'])    
+obj = web_scrape_alt(first_article['link'])    
 paras = obj.find_all('p')
 text = ''
 
@@ -100,7 +114,7 @@ words = tokenize(text)
 FOX_FEED = 'http://feeds.foxnews.com/foxnews/latest'
 feed1 = feedparser.parse(FOX_FEED)
 first_article = feed1['entries'][0]
-obj = web_scrape(first_article['link'])    
+obj = web_scrape_alt(first_article['link'])    
 paras = obj.find_all('p')
 text = ''
 
@@ -120,7 +134,7 @@ REUTERS_SUB = ['/news/artsculture',
 reuters_feed = REUTERS + REUTERS_SUB[0]
 feed2 = feedparser.parse(reuters_feed)
 first_article = feed2['entries'][0]
-obj = web_scrape(first_article['link'])  
+obj = web_scrape_alt(first_article['link'])  
 paras = obj.find_all('p')
 text = ''
 
@@ -136,7 +150,7 @@ words = tokenize(text)
 wired = 'https://www.wired.com/feed'
 feed3 = feedparser.parse(wired)
 first_article = feed3['entries'][1]
-obj = web_scrape(first_article['link'])  
+obj = web_scrape_alt(first_article['link'])  
 paras = obj.find_all('p')
 text = ''
 
