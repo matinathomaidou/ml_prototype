8278cae8f7693c24dadaf1493338f4d8948d481b
David Moore <mooredvd@live.ie>
2017-05-27 11:25:39 +0100

    repo synch. (branch)

--- dbhelper.py
+++ dbhelper.py
@@ -2,13 +2,12 @@
 #-*-coding: utf-8 -*-
 
 import pymongo
-from bson import ObjectId
 import config
 
 
 uri = config.uri
 
-DATABASE = "mlexperience"
+DATABASE = config.DATABASE
 
 
 class DBHelper:
@@ -43,35 +42,5 @@ class DBHelper:
         self.db.users.update({"email": email}, {"$set": {"salt": salt, "hashed":hashed}})
       
 
-    def add_table(self, number, owner):
-        new_id = self.db.tables.insert({"number": number, "owner": owner})
-        return new_id
-
-    def update_table(self, _id, url):
-        self.db.tables.update({"_id": _id}, {"$set": {"url": url}})
-
-    def get_tables(self, owner_id):
-        return list(self.db.tables.find({"owner": owner_id}))
-
-    def get_table(self, table_id):
-        return self.db.tables.find_one({"_id": ObjectId(table_id)})
-
-    def delete_table(self, table_id):
-        self.db.tables.remove({"_id": ObjectId(table_id)})
-
-    def add_request(self, table_id, time):
-        table = self.get_table(table_id)
-        try:
-            self.db.requests.insert({"owner": table['owner'], "table_number": table[
-                                    'number'], "table_id": table_id, "time": time})
-            return True
-        except pymongo.errors.DuplicateKeyError:
-            return False
-
-    def get_requests(self, owner_id):
-        return list(self.db.requests.find({"owner": owner_id}))
-
-    def delete_request(self, request_id):
-        self.db.requests.remove({"_id": ObjectId(request_id)})
         
 
--- news.py
+++ news.py
@@ -3,8 +3,37 @@
 Created on Sat May 20 09:51:51 2017
 
 @author: david
+
+A small program to work with an RSS feeder
+Retrieve news articles
+Process the article into a bag of words
+Perform classification using a trained model
+Find similar articles using TFIDF and GENSIM
+Store the RSS portion of the article, the similar documents and class label in MongoDB
+for later use in a micro service with Flask
+Private research project
+
+Here you will find the HTTP header fields which evenutally get written
+to the target server logs and records who we are 
+https://en.wikipedia.org/wiki/List_of_HTTP_header_fields
+
+Here you will find the urllib request documentation - used for building the http request logic
+https://docs.python.org/3/library/urllib.request.html
+
+Python strip time reference
+http://strftime.org/
+
+string to dates courtsey of
+https://stackoverflow.com/questions/466345/converting-string-into-datetime
+
+Python datetime library
+https://docs.python.org/2/library/datetime.html
+
+
 """
 
+from datetime import datetime
+import pymongo
 import feedparser
 from bs4 import BeautifulSoup
 import urllib.request
@@ -31,20 +60,28 @@ wnl = WordNetLemmatizer()
 from collections import Counter
 import numpy as np
 import pandas as pd
+import config
 #os.environ['STANFORD_PARSER'] = '/Users/David/postagger/stanford-parser-full-2014-08-27/stanford-parser.jar'
 #os.environ['STANFORD_MODELS'] = '/Users/David/postagger/stanford-parser-full-2014-08-27/stanford-parser-3.4.1-models.jar'
 #parser = stanford.StanfordParser(model_path="/Users/David/postagger/stanford-parser-full-2014-08-27/stanford-parser-3.4.1-models/edu/stanford/nlp/models/lexparser/englishPCFG.ser.gz")
 cachedStopWords = stopwords.words("english")
 #cachedStopWords.append('said')
 
+headers = config.headers
+DATABASE = config.DATABASE
+uri = config.uri
 
+client = pymongo.MongoClient(uri)
+db = client[DATABASE]
+
+current_time = datetime.now().replace(hour=0, minute=0, second=0, microsecond=0)
 def web_scrape(web_index):
     base_path= web_index
     web_path = base_path
     print (web_path)
     #Open and read web page. 
     #Parse returned webpage with BeautifulSoup and extract all the tables in it. 
-    request = urllib.request.Request(web_path)
+    request = urllib.request.Request(web_path, headers=headers)
     response = urllib.request.urlopen(request)
     page = response.read().decode('utf-8')        
     soup=BeautifulSoup(page, 'lxml')
@@ -90,37 +127,52 @@ def tokenize(text):
     filter_words = list(filter(lambda token: p.match(token) and len(token)>=min_length, tokens));
     return filter_words      
 
-      
-rec = {}   
-simslist = []   
+ 
 
 BBC_FEED = 'http://feeds.bbci.co.uk/news/rss.xml'
 feed = feedparser.parse(BBC_FEED)
-first_article = feed['entries'][0]
+for f in range(0,len(feed)):
+    rec = {}   
+    simslist = []   
+    first_article = feed['entries'][f]
+    date_pub = datetime.strptime(first_article['published'], '%a, %d %b %Y %H:%M:%S %Z').replace(hour=0, minute=0, second=0)
+    vintage = current_time - date_pub
+    if (vintage.days == 1):
 
-obj = web_scrape_alt(first_article['link'])    
-paras = obj.find_all('p')
-text = ''
+        obj = web_scrape_alt(first_article['link'])    
+        paras = obj.find_all('p')
+        text = ''
 
-for passage in range (12, len(paras)):
-    try:
-        if len(paras[passage].text) > 3:
-            text = text + ' ' + (paras[passage].text)
-    except:
-        continue
+        for passage in range (12, len(paras)):
+            try:
+                if len(paras[passage].text) > 3:
+                    text = text + ' ' + (paras[passage].text)
+            except:
+                continue
  
 
-words = tokenize(text)  
+        words = tokenize(text)  
 
-rec['ml_id'] = 'todo'  #this needs to be a unique incremental number - perhaps MongoDb will help
-rec['link'] = first_article['link']
-rec['title'] = first_article['title']
-rec['summary'] = first_article['summary']
-rec['bagofwords'] = words 
-rec['date'] = first_article['published']
-rec['topic'] = 'todo' #this needs to be the class label from the machine learning model
-rec['simlist'] = simslist #this needs to be a list of similar documents based on tfidf vector and similarity (GENSIM)
- 
+        rec['ml_id'] = 'todo'  #this needs to be a unique incremental number - perhaps MongoDb will help
+        rec['link'] = first_article['link']
+        rec['title'] = first_article['title']
+        rec['summary'] = first_article['summary']
+        rec['bagofwords'] = words 
+        rec['date'] = first_article['published']
+        rec['topic'] = 'todo' #this needs to be the class label from the machine learning model
+        rec['simlist'] = simslist #this needs to be a list of similar documents based on tfidf vector and similarity (GENSIM)
+
+        db.news.insert_one(rec)
+
+    else:
+        print(first_article['title'], ' ', first_article['published'], ' not right vintage')
+        
+
+
+
+
+
+
 
 #this function just removes these prototyped scrapes for the short while
 def others():
