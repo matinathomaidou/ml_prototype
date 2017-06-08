75e3d93e5776d8d4e2beff512b76bd4bdbe2bfdc
David Moore <mooredvd@live.ie>
2017-06-08 21:27:01 +0100

    redis DbHelper class

--- redis_db.py
+++ redis_db.py
@@ -8,32 +8,119 @@ Created on Mon Jun  5 17:31:25 2017
 import redis
 from redis_collections import Dict
 
-email = 'admin@ml'
-email_1 = 'dd'
-email_2 = 'bb'
 #add a new user
 profiles = Dict()
+users = Dict()
+import yaml
 
-profile = {}
-profile['salt'] = 'b'
-profile['name'] = ' '
-profile['city'] = ' '
-profile['news'] = ' '
-profile['currency'] = 'gbp'
-profile['share'] = ' '
-profile['hashed'] = ' '
-profile['admin'] = ' '
+class RedisDBHelper:
 
-profiles[email] = profile
+    def __init__(self):
+        self.db = redis.Redis('localhost')
 
-conn = redis.Redis('localhost')
-conn.hmset("pythonDict", profiles)  
-pythonDict =  (conn.hgetall("pythonDict"))
+    def get_user(self, email):
+        
+            entry = self.db.hget('mluserDict', email)            
+            user = {}
+            user['email'] = email
+            val = yaml.load(entry)
+            user['admin'] = val['admin']
+            user['hashed'] = val['hashed']
+            user['salt'] = val['salt']  
+            return user
+            
 
-#fetch a single email address
-one = conn.hget('pythonDict','admin@mlexperience.org')
+    def add_user(self, email, salt, hashed, is_admin):
+        user = {}
+        user['salt'] = salt
+        user['hashed'] = hashed
+        user['admin'] = is_admin        
+        users[email] = user
+        self.db.hmset("mluserDict", users)  
 
-#delete a user
-two = conn.hdel('pythonDict',email_2)
-pythonDict =  (conn.hgetall("pythonDict"))
-print(pythonDict)
+        profile = {}
+        profile['name'] = ' '
+        profile['city'] = ' '
+        profile['news'] = ' '
+        profile['currency'] = ' '
+        profile['share'] = ' '
+        
+        profiles[email] = profile        
+        
+        self.db.hmset('mlprofileDict',profiles)
+        
+    def list_user(self):
+        users = []
+        mluserDict =  (self.db.hgetall("mluserDict"))        
+        for entry in mluserDict:
+            user = {}
+            user['email'] = entry
+            val = yaml.load(mluserDict[entry])
+            user['admin'] = val['admin']
+            user['hashed'] = val['hashed']
+            user['salt'] = val['salt']
+            users.append(user)
+        return users
+        
+    def del_user(self, email):
+        self.db.hdel('mluserDict',email)
+
+
+    def toggle_admin(self, email):
+       user = self.get_user(email) 
+       val = 'N'
+       if user['admin'] == 'N':
+           val = 'Y'
+       user['admin'] = val
+       users[email] = user   
+       self.db.hmset("mluserDict", users)  
+        
+
+    def pw_user_update(self, email, salt, hashed, is_admin):
+        id = self.get_user(email)
+        id['salt'] = salt
+        id['hashed'] = hashed
+        users[email] = id   
+        self.db.hmset("mluserDict", users)  
+      
+
+    def user_profile_read(self, email):
+        profiler_r = self.db.user_profiles.find_one({'email': email})
+        try:
+            if profiler_r['profile']:
+                return profiler_r['profile']
+        except:
+            profile = {}
+            profile['name'] = ' '
+            profile['city'] = ' '
+            profile['news'] = ' '
+            profile['currency'] = ' '
+            profile['share'] = ' '
+            self.db.user_profiles.insert_one({'email': email, 'profile' : profile})
+            return profile
+            
+    
+
+    def user_profile_update(self, email, profile):
+        self.db.user_profiles.update({'email': email}, {'$set' : {'profile' : profile}})
+        
+    def user_delete(self, email):
+        self.db.users.delete_many({'email' : email})
+        self.db.user_profiles.delete_many({'email' : email})
+    
+    def read_news(self):
+        news = []
+        for art in self.db.news.find():
+            article = {}
+            article['link'] = art['link']
+            article['title'] = art['title']
+            article['summary'] = art['summary']
+            article['date'] = art['date']
+            article['topic'] = art['topic']
+            article['id'] = art['ml_id']
+            news.append(article)
+        return news
+        
+    def push_feed_back(self, fed_back):
+        self.db.feedback.insert({'Feedback': fed_back})
+        
